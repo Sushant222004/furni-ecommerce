e91d11b23620631de7ae3ff3810a927781b4325f
Sushant <sushant22102004@gmail.com>
2026-08-02 20:37:45 +0530

    Fix controller

--- furni-server/controllers/authcontroller.js
+++ furni-server/controllers/authcontroller.js
@@ -1,4 +1,4 @@
-const User = require("../models/User");
+const User = require("../models/user");
 const bcrypt = require("bcryptjs");
 const { success, error } = require("../utils/responsehandler");
 const jwt = require("jsonwebtoken");

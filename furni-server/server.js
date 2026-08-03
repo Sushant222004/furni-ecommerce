db8188b5d39b185796f6a339a6aac2a002a8aca2
Sushant <sushant22102004@gmail.com>
2026-08-03 10:46:56 +0530

    Use deployed backend

--- furni-server/server.js
+++ furni-server/server.js
@@ -18,7 +18,7 @@ dotenv.config();
     cors({
       origin: [
         "http://localhost:5173",
-        "https://furni-ecommerce-iota.vercel.app/",
+        "https://furni-ecommerce-iota.vercel.app",
       ],
       credentials: true,
     }),

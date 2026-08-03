02ea29f1b46384b98ba9cccda3ab818a879b0307
Sushant <sushant22102004@gmail.com>
2026-08-03 12:39:23 +0530

    Update CORS for new Vercel domain

--- furni-server/server.js
+++ furni-server/server.js
@@ -18,7 +18,7 @@ dotenv.config();
     cors({
       origin: [
         "http://localhost:5173",
-        "https://furni-ecommerce-iota.vercel.app",
+        "https://furni-ecommerce-tsn4.vercel.app",
       ],
       credentials: true,
     }),
